fdf52b1cd252c2023ac1a16fd76a3f0db7a2ea73
Venkatesh Subramani <52658916+venk351@users.noreply.github.com>
2023-11-02 12:43:48 +0530

    Update and rename Find_Each_Occurrence_of_numbers to Find_Each_Occurrence_of_numbers.java

--- Find_Each_Occurrence_of_numbers.java
+++ Find_Each_Occurrence_of_numbers.java
@@ -4,7 +4,7 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public class Interview1 {
+public class Find_Each_Occurrence_of_numbers {
 	public static void main(String[] args) {
 		List<Integer> arr = Arrays.asList(1,1,1,2,3,4,4,2,3);
 		Map<Integer,Long> map = arr.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
